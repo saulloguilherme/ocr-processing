6db024c8456d81a70c1bbe4bf93dd2770952bb87
Saullo Guilherme <saullo.guilherme7@gmail.com>
2026-02-23 22:06:47 -0300

    add: minio and image verification

--- ocr-api/src/main/java/com/saulloguilherme/ocr_api/service/FileValidationService.java
+++ ocr-api/src/main/java/com/saulloguilherme/ocr_api/service/FileValidationService.java
@@ -1,9 +1,37 @@
 package com.saulloguilherme.ocr_api.service;
 
+import com.saulloguilherme.ocr_api.exception.BadRequestException;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class ImageValidationService {
+public class FileValidationService {
 
+    private final int maxUploadSizeInMb = 5 * 1024 * 1024; // 5 MegaBytes
+
+    public void validate(MultipartFile file) {
+        validateSize(file);
+        validateExtension(file);
+    }
+
+    private void validateContent(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new BadRequestException("O arquivo está vazio.");
+        }
+    }
+
+    private void validateSize(MultipartFile file) {
+        if (file.getSize() > maxUploadSizeInMb) {
+            throw new BadRequestException("Tamanho máximo excedido.");
+        }
+    }
+
+    private void validateExtension(MultipartFile file) {
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        if (!"png".equals(extension) && !"jpeg".equals(extension) && !"jpg".equals(extension)) {
+            throw new BadRequestException("O tipo do arquivo não é permitido.");
+        }
+    }
 
 }
--- ocr-api/src/main/java/com/saulloguilherme/ocr_api/service/MinioService.java
+++ ocr-api/src/main/java/com/saulloguilherme/ocr_api/service/MinioService.java
@@ -1,16 +1,65 @@
 package com.saulloguilherme.ocr_api.service;
 
+import com.saulloguilherme.ocr_api.exception.BadRequestException;
+import io.minio.GetPresignedObjectUrlArgs;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.http.Method;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
 public class MinioService {
+
+    @Value("${MINIO_BUCKET}")
+    private String bucket;
+
+    @Autowired
+    private FileValidationService fileValidationService;
 
     @Autowired
     private MinioClient minioClient;
 
     public String save(MultipartFile file) {
-        minioClient.;
+        fileValidationService.validate(file);
+
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        String name = UUID.randomUUID() + "." + extension;
+
+        try (InputStream is = file.getInputStream()) {
+            minioClient.putObject(
+                    PutObjectArgs.builder().bucket(bucket)
+                            .object(name)
+                            .stream(is, file.getSize(), -1)
+                            .contentType(file.getContentType())
+                            .build());
+        } catch (Exception e) {
+            throw new BadRequestException("Falha ao salvar arquivo no MinIO");
+        }
+        return name;
+    }
+
+    public String getPresignedUrl(String objectName, int expiryInSeconds) {
+        try {
+            minioClient.getPresignedObjectUrl(
+                    GetPresignedObjectUrlArgs.builder()
+                            .bucket(bucket)
+                            .object(objectName)
+                            .method(Method.GET)
+                            .expiry(expiryInSeconds)
+                            .build()
+            );
+        } catch (Exception e) {
+            throw new BadRequestException("Falha ao encontrar arquivo no MinIO");
+        }
+
+        return "";
     }
 
 }
